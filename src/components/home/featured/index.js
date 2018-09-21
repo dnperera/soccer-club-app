43b51241dc08b8b146bdd9d4f83ff8dc15f0f64e
Dayan Perera <dayannp@gmail.com>
2018-09-21 12:56:59 -0700

    add animations to featured section using React-Move

--- src/components/home/featured/index.js
+++ src/components/home/featured/index.js
@@ -1,6 +1,13 @@
 import React from "react";
+import Stripes from "./Stripes";
+import Text from "./Text";
 
 const Featured = () => {
-  return <div className="featured_wrapper">Featured Area...</div>;
+  return (
+    <div className="featured_wrapper">
+      <Stripes />
+      <Text />
+    </div>
+  );
 };
 export default Featured;
